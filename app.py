e4c9d3a2548437e7fb0efb57ae616e35e3db28f3
PabloRC0525 <121038827@upq.edu.mx>
2024-04-23 00:35:24 -0600

    Clientes
    Se agregaron las vistas de confirmación para eliminación de clientes y edición de clientes, también las rutas para que estas funcionen

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # Configuración de la base de datos
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Rolonlol135'
-app.config['MYSQL_DB'] = 'chacharitaspi'
 
-mysql = MySQL(app)
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']=""
+app.config['MYSQL_DB']="chacharitaspi"
+
+app.secret_key='mysecretkey'
+
+
+mysql= MySQL(app)
 
 # Ruta para mostrar todos los clientes
 @app.route('/')
@@ -18,7 +22,7 @@ def index():
     cur.execute("SELECT * FROM clients")
     clients = cur.fetchall()
     cur.close()
-    return render_template('index.html', clients=clients)
+    return render_template('clients/index.html', clients=clients)
 
 # Ruta para agregar un nuevo cliente
 @app.route('/clients/add', methods=['POST'])
@@ -37,15 +41,52 @@ def add_client():
         mysql.connection.commit()
         cur.close()
         return redirect(url_for('index'))
+    
+# Vista para mostrar la página de confirmación de eliminación
+@app.route("/delete/<int:id>")
+def mostrar_confirmacion_eliminacion(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM clients WHERE id = %s', (id,))
+    client = cur.fetchone()
+    cur.close()
+    return render_template('clients/delete.html', clients=client)
 
 # Ruta para eliminar un cliente
-@app.route('/clients/delete/<int:id>')
+@app.route('/deletec/<int:id>', methods=['POST'])
 def delete_client(id):
     cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM subscriptions WHERE id_client = %s", (id,))
     cur.execute("DELETE FROM clients WHERE id = %s", (id,))
     mysql.connection.commit()
     cur.close()
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/visualizaredit/<string:id>')
+def visualizaredit(id):
+    cur = mysql.connection.cursor()
+    cur.execute('select * from clients where id = %s', (id,))
+    clients = cur.fetchone()
+    return render_template('/clients/edit.html', clients=clients)
+
+# Ruta para mostrar un cliente específico para edición
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_client(id):
+    if request.method == 'POST':
+        name = request.form['name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        password = request.form['password']
+        id_gender = request.form['id_gender']
+        id_address = request.form['id_address']
+
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE clients SET name = %s, last_name = %s, email = %s, phone_number = %s, password = %s, id_gender = %s, id_address = %s WHERE id = %s", (name, last_name, email, phone_number, password, id_gender, id_address, id))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('index'))
+    
+    
+#Ejecucion de servidor
+if __name__ =='__main__':
+    app.run(port=3000,debug=True)
